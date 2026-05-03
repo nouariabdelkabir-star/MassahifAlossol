b464d18d74cd1d42625afad1b80b34ae751a97fe
NouariAbdelkabir <nouari.abdelkabir@gmail.com>
2026-05-03 02:25:52 +0100

    Add direct URL construction for PDF files

--- js/script.js
+++ js/script.js
@@ -202,6 +202,8 @@ function getRealPage(displayPage, startPage) {
 function buildViewerUrl(pdfPath, realPage) {
     const baseUrl = "https://massahifalossol-nouari.github.io/MassahifAlossol/";
     const fullPath = window.location.origin + '/' + pdfPath;
+    const fileName = pdfPath.split('/').pop(); // qalun1.pdf
+    const directUrl = `https://massahifalossol-nouari.github.io/MassahifAlossol/pdfs/${fileName}`;
     const encodedPath = encodeURIComponent(fullPath);
     return `pdfjs/web/viewer.html?file=${encodedPath}#page=${realPage}`;
 }
